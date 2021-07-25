174dbae95f1816360163e6ed2fa89462c552078b
Eduarda Trindade <eduardaescouto@gmail.com>
2021-07-25 19:53:37 -0300

    Trabalho Eduarda Escouto

---
+++ 2.trabalho_calculadora/trabalho_3_eduarda_escouto/android/app/src/main/kotlin/com/example/trabalho_3_eduarda_escouto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trabalho_3_eduarda_escouto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 2.trabalho_calculadora/trabalho_3_eduarda_escouto/lib/Calculadora.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class Calculadora extends StatefulWidget {
+  @override
+  State createState() => new CalculadoraState();
+}
+
+class CalculadoraState extends State<Calculadora> {
+  // Métodos de cálculos
+
+  // Método build - constroi a estrutura do app
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(title: new Text("** Calculadora Simples **")),
+      body: new Container(
+        padding: const EdgeInsets.all(40.0),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Resultado do cálculo
+            new Text(
+              "Resultado: $resultado",
+              style: new TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.pink),
+            ),
+
+            // Campo de Texto (input 1)
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Informe o valor 1"),
+              controller: campoValor1,
+            ),
+
+            // Campo de texto (input 2)
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Informe o valor 2"),
+              controller: campoValor2,
+            ),
+
+            // Espaçamento depois dos inputs
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            // Nova linha(row)
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text(
+                    "+",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.purple,
+                  onPressed: somar,
+                ),
+                new MaterialButton(
+                  child: new Text(
+                    "-",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.purple,
+                  onPressed: subtrair,
+                )
+              ],
+            ),
+
+            new Padding(padding: const EdgeInsets.only(top: 20)),
+
+            // Nova linha(row)
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text(
+                    "*",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.purple,
+                  onPressed: multiplicar,
+                ),
+                new MaterialButton(
+                  child: new Text(
+                    "/",
+                    style: new TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  color: Colors.purple,
+                  onPressed: dividir,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //Atributos
+  var valor1;
+  var valor2;
+  var resultado = 0;
+
+  TextEditingController campoValor1 = new TextEditingController(text: "");
+  TextEditingController campoValor2 = new TextEditingController(text: "");
+
+//Métodos usados para calcular
+  void somar() {
+    setState(() {
+      valor1 = num.parse(campoValor1.text);
+      valor2 = num.parse(campoValor2.text);
+      resultado = valor1 + valor2;
+    });
+  }
+
+  void subtrair() {
+    setState(() {
+      valor1 = num.parse(campoValor1.text);
+      valor2 = num.parse(campoValor2.text);
+      resultado = valor1 - valor2;
+    });
+  }
+
+  void multiplicar() {
+    setState(() {
+      valor1 = num.parse(campoValor1.text);
+      valor2 = num.parse(campoValor2.text);
+      resultado = valor1 * valor2;
+    });
+  }
+
+  void dividir() {
+    setState(() {
+      valor1 = num.parse(campoValor1.text);
+      valor2 = num.parse(campoValor2.text);
+      resultado = valor1 / valor2;
+    });
+  }
+}
